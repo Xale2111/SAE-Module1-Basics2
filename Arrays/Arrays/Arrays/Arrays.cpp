dab0b3a60d0c43b053b25146abf5704688d766e6
Alex King <alexandreking.dev@gmail.com>
2025-09-10 11:06:57 +0200

    Check if number exist in random array completed

--- Arrays/Arrays/Arrays/Arrays.cpp
+++ Arrays/Arrays/Arrays/Arrays.cpp
@@ -18,7 +18,101 @@
 using std::cout;
 using std::cin;
 
-const int arraySize = 5;
+const int arraySize = 10;
+void InitRandomArray();
+bool CheckIfNumberIsInArrayWithForLoop(int userNumber);
+bool CheckIfNumberIsInArrayWithFind(int userNumber);
+
+
+
+int randomNumberArray[arraySize];
+
+int main()
+{
+	InitRandomArray();
+	int userInput;
+	do
+	{
+		cout << "Enter a number between 0 and 20 : ";
+		cin >> userInput;
+	}
+	while (userInput < 0 || userInput > 20);
+	cout << "Random array was : \n";
+	for (int i = 0; i < arraySize; ++i)
+	{
+		cout << randomNumberArray[i] << "\n";
+	}
+
+	cout << "\n\n -------------------USING FOR LOOP METHOD-----------------";
+
+
+	if (CheckIfNumberIsInArrayWithForLoop(userInput))
+	{
+		cout << "\n\nYour number is in the array !! NOICE";
+	}
+	else
+	{
+		cout << "\n\nYour number is NOT in the array ! Schade";
+	}
+
+	cout << "\n\n -------------------USING FIND METHOD-----------------";
+
+	if (CheckIfNumberIsInArrayWithFind(userInput))
+	{
+		cout << "\n\nYour number is in the array !! NOICE";
+	}
+	else
+	{
+		cout << "\n\nYour number is NOT in the array ! Schade";
+	}
+
+	int temp;
+	cin >> temp;
+
+}
+
+
+void InitRandomArray()
+{
+	for (int i = 0; i < arraySize; ++i)
+	{
+		srand((time(NULL) + rand()));
+		randomNumberArray[i] = (rand() % 20) + 1;
+	}
+}
+
+
+bool CheckIfNumberIsInArrayWithForLoop(int userNumber)
+{
+	for (int i = 0; i < arraySize; ++i)
+	{
+		if (userNumber == randomNumberArray[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+
+bool CheckIfNumberIsInArrayWithFind(int userNumber)
+{
+	if (std::any_of(std::begin(randomNumberArray), std::end(randomNumberArray), [=](int n) {return n == userNumber; })) 
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+//
+//FIRST EXERCICE
+//
+
+/*const int arraySize = 5;
 
 int ReturnHighestNumberInArray(int intArray[]);
 
@@ -40,6 +134,6 @@ int ReturnHighestNumberInArray(int intArray[])
 {
 	std::sort(intArray, intArray + arraySize, std::greater<int>());
 	return intArray[0];
-}
+}*/
 
 
