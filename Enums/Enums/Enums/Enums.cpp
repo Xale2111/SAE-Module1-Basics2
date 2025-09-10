16614c1f111dc6e3165290a30576f71d2e337771
Alex King <alexandreking.dev@gmail.com>
2025-09-10 10:31:41 +0200

    Working on the enum exercice. Not working, having some troubles using the enum

--- Enums/Enums/Enums/Enums.cpp
+++ Enums/Enums/Enums/Enums.cpp
@@ -1,7 +1,52 @@
 
+#include <algorithm>
 #include <iostream>
+
+using std::cout;
+using std::cin;
+
+
+enum Animal {
+    Cow,
+    Duck,
+    Dog,
+    Cat,
+};
+
+void MakeAnimalScream(Animal animal);
+
+const int numberOfAnimal = 4;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string userInput = "";
+    cout << "Type an animal you want to hear scream : ";
+    cin >> userInput;
+
+    MakeAnimalScream(Cow);
+}
+
+void MakeAnimalScream(Animal animal)
+{
+	switch (animal)
+	{
+	    case Cow:
+            cout << "MMEEEEEEEUUUUUUUUHHHHH";
+	        break;
+        case Duck:
+            cout << "Coin Coin";
+            break;
+        case Dog:
+            cout << "Wouaf Wouaf";
+            break;
+        case Cat:
+            cout << "Miaouw Miaouw";
+            break;
+        case CW_DEFAULT:
+            cout << ".... oooh wait, this animal doesn't make a sound or isn't listed... sorry";
+            break;
+
+	} 
 }
